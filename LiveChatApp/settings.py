3850be79e67121044eb0d1f91c99ed1695144c1d
Sahil Handa <handasahil300@gmail.com>
2024-05-23 12:08:15 +0530

    Websocket Features

--- LiveChatApp/settings.py
+++ LiveChatApp/settings.py
@@ -148,7 +148,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "handasahil300@gmail.com"
-EMAIL_HOST_PASSWORD = "eehk flws ogni ctmr"
+EMAIL_HOST_PASSWORD = "plyr xxct redy htoa"
+
 
 
 # ASGI Configuration
