e6b1ce202c9e684ad1c60ad21d90fb5b86ee306f
SahilHanda300 <109428092+SahilHanda300@users.noreply.github.com>
2024-07-06 22:37:11 +0530

    Update settings.py

--- LiveChatApp/settings.py
+++ LiveChatApp/settings.py
@@ -148,7 +148,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "handasahil300@gmail.com"
-EMAIL_HOST_PASSWORD = "rszp vthy qrsm sgna"
+EMAIL_HOST_PASSWORD = "wgbb vggf exka dmsj"
 
 
 
